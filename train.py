9619e7ab2e378a1cabae33c964da3271db8d2c2e
Lurzapps <lurzapps@gmail.com>
2021-02-24 15:19:27 +0100

    update train amount

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from utils.chess_utils import *
 df = pd.read_csv('data/games.csv', sep=';')
 df = df[df['winner'] != 'draw']
 df = df[df['victory_status'] != 'outoftime']
-moves = df['moves'].values[:100]
+moves = df['moves'].values
 winner = df['winner'].values
 X = []
 y = []
@@ -25,8 +25,6 @@ for game in moves:
     else:
         game_winner = 1
 
-    print(game_winner)
-
     board = chess.Board()
     for i in range(len(all_moves)):
         board.push_san(all_moves[i])
@@ -36,40 +34,37 @@ for game in moves:
         X.append([rows])
         y.append(value)
 
-    print('x:', X)
-    print('y:', y)
-
-    break
-
     index += 1
 
-# # define X and y
-# X = np.array(X).reshape((len(X), 8, 8, 12))
-# y = np.array(y)
-#
-# # start training
-# # set name of files
-# h5 = 'model.h5'
-# json = 'model.json'
-#
-# # create callbacks
-# checkpoint = keras.callbacks.ModelCheckpoint(h5,
-#                                              monitor='loss',
-#                                              verbose=0,
-#                                              save_best_only=True,
-#                                              save_weights_only=True,
-#                                              mode='auto',
-#                                              period=1)
-# es = keras.callbacks.EarlyStopping(monitor='loss', mode='min', verbose=1, patience=500)
-# callback = [checkpoint, es]
-# model_json = model.to_json()
-#
-# with open(json, "w") as json_file:
-#     json_file.write(model_json)
-#
-# print('Training Network...')
-# history = model.fit(X, y, epochs=1000, verbose=2, callbacks=callback)
-#
-# # plot and save
-# plt.plot(history.history['loss'])
-# plt.savefig('history.png')
+print(index)
+
+# define X and y
+X = np.array(X).reshape((len(X), 8, 8, 12))
+y = np.array(y)
+
+# start training
+# set name of files
+h5 = 'model.h5'
+json = 'model.json'
+
+# create callbacks
+checkpoint = keras.callbacks.ModelCheckpoint(h5,
+                                             monitor='loss',
+                                             verbose=0,
+                                             save_best_only=True,
+                                             save_weights_only=True,
+                                             mode='auto',
+                                             period=1)
+es = keras.callbacks.EarlyStopping(monitor='loss', mode='min', verbose=1, patience=500)
+callback = [checkpoint, es]
+model_json = model.to_json()
+
+with open(json, "w") as json_file:
+    json_file.write(model_json)
+
+print('Training Network...')
+history = model.fit(X, y, epochs=1000, verbose=2, callbacks=callback)
+
+# plot and save
+plt.plot(history.history['loss'])
+plt.savefig('history.png')
